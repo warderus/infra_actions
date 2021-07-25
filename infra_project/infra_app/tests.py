21134b36021a78e0e6e8cbbb8ea0302aa331038e
Warderus <Varderus@yandex.ru>
2021-07-25 21:50:38 +0300

    add

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -19,6 +19,5 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
